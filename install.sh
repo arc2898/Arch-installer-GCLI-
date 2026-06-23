c06929fb118a9da0fe049cdcb74375e98e901e6f
arc2898 <rc002898@gmail.com>
2026-06-23 11:43:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-echo 'Arch Installer GCLI'
+
+clear
+echo "================================="
+echo "      Arch Installer GCLI"
+echo "================================="
+
+source ui/menu.sh
+source core/hardware.sh
+source core/disk_detect.sh
+
+main_menu
